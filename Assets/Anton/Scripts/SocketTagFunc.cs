590253d167d491d49426a2aecc3ffc1370704c96
AntonBuus <antonbuushansen@gmail.com>
2024-10-16 09:25:57 +0200

    VR setup, grab, move

---
+++ Assets/Anton/Scripts/SocketTagFunc.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SocketTagFunc : MonoBehaviour
+{
+
+    public GameObject gameObject;
+    
+    public int number = 1; 
+
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
