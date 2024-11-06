3a20986323810e7de26e16ef87a6507cd8c27e20
AntonBuus <antonbuushansen@gmail.com>
2024-11-06 14:11:30 +0100

    Sockeinteractor scene changes

--- Assets/_Anton/Scripts/SocketTagFunc.cs
+++ Assets/_Anton/Scripts/SocketTagFunc.cs
@@ -1,24 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-
-public class SocketTagFunc : MonoBehaviour
+// From Valem Tutorials: https://youtu.be/pMOHX1qD2bE?si=eKu7ToWeINVklKg3
+public class SocketTagFunc : XRSocketInteractor
 {
+    public string targetTag;
 
-    //public GameObject gameObject;
-    
-    public int number = 1; 
-
-
-    void Start()
+    public override bool CanHover(IXRHoverInteractable interactable)
     {
-        
+        return base.CanHover(interactable) && interactable.transform.tag == targetTag;
     }
 
-
-    void Update()
+    public override bool CanSelect(IXRSelectInteractable interactable)
     {
-        
+        return base.CanSelect(interactable) && interactable.transform.tag == targetTag;
     }
 }
+
