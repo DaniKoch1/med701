0628166160d3032e337838a5dce60b17dbe79ac5
AntonBuus <antonbuushansen@gmail.com>
2024-11-22 15:39:26 +0100

    Relocate items and clipboard iteration in xrsetup

---
+++ Assets/_Anton/Scripts/ReturnInteractables.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnInteractables : MonoBehaviour
+{
+    private Dictionary<GameObject, Vector3> originalPositions = new Dictionary<GameObject, Vector3>();
+    public LayerMask interactableLayer;
+
+    void Start()
+    {
+        GameObject[] allObjects = FindObjectsOfType<GameObject>();
+        foreach (GameObject obj in allObjects)
+        {
+            if ((interactableLayer.value & (1 << obj.layer)) != 0)
+            {
+                originalPositions[obj] = obj.transform.position;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider _interactableToReturn)
+    {
+        if ((interactableLayer.value & (1 << _interactableToReturn.gameObject.layer)) != 0 && originalPositions.ContainsKey(_interactableToReturn.gameObject))
+        {
+            _interactableToReturn.transform.position = originalPositions[_interactableToReturn.gameObject];
+        }
+    }
+    
+}
